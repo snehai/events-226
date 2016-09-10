ecbcd19623152eebd045791de3473d5faf9508b0
snehai <snehailangovan@gmail.com>
2016-09-10 14:18:10 -0700

    Add files via upload
    added more rows to events table

--- events.sql
+++ events.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 09, 2016 at 12:37 AM
+-- Host: localhost
+-- Generation Time: Sep 10, 2016 at 11:16 PM
 -- Server version: 10.1.16-MariaDB
--- PHP Version: 5.6.24
+-- PHP Version: 5.5.38
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `events`
+-- Database: `eventsdb`
 --
 
 -- --------------------------------------------------------
@@ -26,9 +26,8 @@ SET time_zone = "+00:00";
 -- Table structure for table `events`
 --
 
-
 CREATE TABLE `events` (
-  `event_id` int(11) NOT NULL PRIMARY KEY,
+  `event_id` int(11) NOT NULL,
   `event_name` varchar(50) NOT NULL,
   `event_address` varchar(100) NOT NULL,
   `event_date` datetime NOT NULL,
@@ -48,8 +47,25 @@ INSERT INTO `events` (`event_id`, `event_name`, `event_address`, `event_date`, `
 (3, 'therapy: you''re good enough', 'Breather Room - 55 New Montgomery St, San Francisco, CA 94105', '2016-09-17 10:30:00', 0, 'Health', 1, 'we''re going to be talking about feeling like we always have to do more, more, more for our jobs, about keeping up with everyone else, and about learning to do what makes us happy instead of doing passion projects to make employers happy'),
 (4, 'MIDNIGHT CRUISE | October Bash', 'Pier 40 San Francisco - 40 Pier, San Francisco, CA 94107', '2016-10-01 12:00:00', 1, 'Entertainment', 1, '2016 Party Cruise Series, aboard Fume Blanc with World Class House Music & Open Format DJs'),
 (5, 'Dinner, Movie Under the Stars & Fundraiser', 'Glendale, CA', '2016-09-24 17:00:00', 1, 'Charity', 1, 'Come meet new friends and form new relationships. Take a break from your busy life; you deserve a little treat here and there. ;)'),
-(6, 'The Beerwalk - Willow Glen', 'Willow Glen - San Jose, CA 95125', '2016-10-29 15:00:00', 0, 'Food', 1, 'Stroll Lincoln Avenue in Downtown Willow Glen and discover all the shops, art galleries, and great restaurants. All while sipping on great beers and enjoying the social enthusiasm of shopkeepers and beer lovers, alike.');
+(6, 'The Beerwalk - Willow Glen', 'Willow Glen - San Jose, CA 95125', '2016-10-29 15:00:00', 0, 'Food', 1, 'Stroll Lincoln Avenue in Downtown Willow Glen and discover all the shops, art galleries, and great restaurants. All while sipping on great beers and enjoying the social enthusiasm of shopkeepers and beer lovers, alike.'),
+(7, 'San Francisco Fringe Festival Opening Party', '1700 E Bayshore Rd, East Palo Alto, CA 94303', '2016-09-30 00:00:00', 1, NULL, 1, 'Things are kicking off at PianoFight''s opening night party for SF''s beloved annual Fringe Festival, now celebrating its 25th year!'),
+(8, 'San Francisco Fringe Festival Opening Party', '1700 E Bayshore Rd, East Palo Alto, CA 94303', '2016-09-30 00:00:00', 1, NULL, 1, 'Things are kicking off at PianoFight''s opening night party for SF''s beloved annual Fringe Festival, now celebrating its 25th year!'),
+(9, 'TRAVISWILD''s Animal Birthday!', '811 E Arques Ave, Sunnyvale, CA 94085\r\n', '2016-09-21 00:00:00', 0, 'Entertainment', 1, 'DJ TRAVISWILD has shared the stage with A-Trak, Thomas Jack, Chromeo, Viceroy, and more, AND he''s celebrating his birthday in SF ft. sets by Deep Moisture and his fans The Animal Kingdom.'),
+(10, 'WasTED Talks', '3255 Mission College Blvd\r\n(408) 961-3599', '2016-10-13 00:00:00', 0, 'Health', 1, 'Just like TED Talks, except the speakers are WASTED.'),
+(11, 'Mortified Oakland', '780 E El Camino Real\r\n(408) 738-9430', '2016-12-16 00:00:00', 0, 'Tech', 1, 'The New Parish\r\nMortified is a comedy show where grown-a** adults share their most embarrassing and real childhood letters, lyrics, journals, home movies, and artwork... in front of total strangers.'),
+(12, 'teamLab: Living Digital Space and Future Parks', 'Pacific Rim Shopping Center\r\n20011 Bollinger Rd\r\n(408) 973-8402', '2016-10-26 00:00:00', 1, 'Tech', 1, 'Pace Art + Technology\r\nYour Instagram feed may already be filled with images from this place, but you should see this immersive, large-scale, site-specific installation—and digital playground for all ages—for yourself!'),
+(13, '21st Annual Ghirardelli Chocolate Festival', '112 N Rengstorff Ave\r\n(650) 969-5784', '2016-11-16 00:00:00', 0, 'Food', 1, 'Ghirardelli Square\r\nGet chocolate wasted at the 21st Annual Ghirardelli Chocolate Festival, held in Ghirardelli Square! There will be live culinary demos, ice cream eating contests, chocolate tastings, and much more!'),
+(14, 'Nightlife at CAL Academy', '1399 W San Carlos St\r\n(408) 971-3098', '2016-12-15 00:00:00', 1, 'Education', 1, 'Nightlife at CAL Academy: Claude’s 21st Birthday Bash | Music by Mom DJs');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`event_id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
